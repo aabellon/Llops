2fb1cdb134686b02edd44c682baa28df80dfd002
aabellon <aabellon@ies-sabadell.cat>
2019-12-04 19:40:33 +0100

    canvis

--- src/main/java/Projecte/Llops/Controller/RolJugadorPartida.java
+++ src/main/java/Projecte/Llops/Controller/RolJugadorPartida.java
@@ -1,5 +1,68 @@
 package Projecte.Llops.Controller;
 
-public class RolJugadorPartida {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "RolJugadorPartida")
+public class RolJugadorPartida {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "user")
+	private User user;
+	@Column(name = "rol")
+	private Rol rol;
+	@Column(name = "partida")
+	private Partida partida;
+
+	public RolJugadorPartida() {
+		super();
+	}
+
+	public RolJugadorPartida(int id, User user, Rol rol, Partida partida) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.rol = rol;
+		this.partida = partida;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+	public Partida getPartida() {
+		return partida;
+	}
+
+	public void setPartida(Partida partida) {
+		this.partida = partida;
+	}
 }
+
