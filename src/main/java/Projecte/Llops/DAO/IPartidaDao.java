991ac8b048a188fb9aa14486f5da2ac0f1389887
oriol.porta <oriol.porta@hotmail.com>
2019-12-19 16:59:58 +0100

    unirse hecho

--- src/main/java/Projecte/Llops/DAO/IPartidaDao.java
+++ src/main/java/Projecte/Llops/DAO/IPartidaDao.java
@@ -6,7 +6,7 @@ import Projecte.Llops.model.Partida;
 
 //En la interficie siemplemente extendemos IGenericDao y despejamos los genéricos
 public interface IPartidaDao extends IGenericDao<Partida,Integer>{
-
+	
 	void saveOrUpdate(Partida m);
 
 	Partida get(Integer id);
