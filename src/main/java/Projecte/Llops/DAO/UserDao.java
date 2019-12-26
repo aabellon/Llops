55e87880b40550f0b6a4b3e7e0eae1f2b4522ef6
dis354 <goresuke@gmail.com>
2019-12-26 12:18:17 +0100

    Me cago en el puto git

--- src/main/java/Projecte/Llops/DAO/UserDao.java
+++ src/main/java/Projecte/Llops/DAO/UserDao.java
@@ -6,10 +6,38 @@ import Projecte.Llops.model.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+ import Projecte.Llops.model.Partida;
+import Projecte.Llops.model.User;
 //El modulo no necesita nada que no lo trate el genérico así que lo podemos dejar vacío
 public class UserDao extends GenericDao<User,Integer> implements IUserDao {
 	 Scanner Scaner = new Scanner (System.in);
-	
+	 public boolean afegirPartida(User u, Partida part) {
+
+	 		Session session = sessionFactory.getCurrentSession();
+
+	 		try {
+				session.beginTransaction();
+				session.saveOrUpdate(u);
+				session.saveOrUpdate(part);
+
+	 			session.getTransaction().commit();
+				return true;
+			} catch (HibernateException e) {
+				e.printStackTrace();
+				if (session != null && session.getTransaction() != null) {
+					System.out.println("\n.......Transaction Is Being Rolled Back.......");
+					session.getTransaction().rollback();
+				}
+				e.printStackTrace();
+				return false;
+
+	 		}
+
+
+	 	}
 
 
 	@Override
@@ -45,5 +73,6 @@ public class UserDao extends GenericDao<User,Integer> implements IUserDao {
 		user.setPassword(pw);
 
 	}
+	
 	}
 	
